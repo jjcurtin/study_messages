f78c6e980a26299ff3c019a21614588818105413
Coco-Jiachen-Yu <jyu274@wisc.edu>
2024-10-08 01:23:31 -0500

    paper revise

--- _chtc/mak_jobs.R
+++ _chtc/mak_jobs.R
@@ -3,7 +3,7 @@
 library(tidyverse) 
 devtools::source_url("https://github.com/jjcurtin/lab_support/blob/main/chtc/fun_make_jobs.R?raw=true")
 
-path_training_controls <- file.path("_chtc/training_controls_messages.R") 
+path_training_controls <- file.path("_chtc/training_controls_messages_concat.R") 
 make_jobs(path_training_controls, overwrite_batch = FALSE)
 
 
